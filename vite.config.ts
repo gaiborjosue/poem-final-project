86c4c003762e98f9b1292c5bcd7705c96b619728
gaiborjosue <gaiborjimenezjosue@gmail.com>
2025-12-15 21:44:59 -0500

    deployments

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages base path
+  base: '/poem-final-project/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
